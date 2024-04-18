a25abd0581a12fba40ad6c491c940e8d64ae91f5
TylerChen1200 <TylerChen1200@gmail.com>
2024-04-17 23:14:37 -0400

    a6 Mongo su23

--- kanbas-node-server-app/App.js
+++ kanbas-node-server-app/App.js
@@ -1,15 +1,20 @@
-    import Hello from "./Hello.js";
-    import Lab5 from "./Lab5.js";
-    import express from 'express';
-    import cors from "cors";
-    const app = express();
-    
-    app.use(cors());
-    app.use(express.json());
-    Lab5(app);
-    Hello(app);
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
+import express from 'express';
+import cors from "cors";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+const app = express();
 
-    
-    app.get('/', (req, res) => res.send('Hello World!'));
-    
-    app.listen(process.env.PORT || 4000);
+app.use(cors());
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
+Lab5(app);
+Hello(app);
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const port = 4000;
+app.listen(process.env.PORT || 4000);
---
+++ kanbas-node-server-app/Kanbas/Database/courses.js
@@ -0,0 +1,6 @@
+export default [ { "_id": "RS101", "name": "Rocket Propulsion", "number": "RS4550", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image": "desert.jpg"  },
+  { "_id": "SAC103", "name": "Failure in Film", "number": "RS4560", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image": "desert.jpg"  },
+  { "_id": "MOI402", "name": "Moist Disney+", "number": "RS4570", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image": "desert.jpg"  } ]
